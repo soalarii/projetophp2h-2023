c67d8b25cf65070a9d734f399e39a4ec210cebee
soalarii <soaresmachadolarissa@gmail.com>
2023-08-09 19:47:08 -0300

    Criando segundo arquivo

--- exemplo01.php
+++ exemplo01.php
@@ -4,5 +4,6 @@
 	$num2 = 30;
 	$resp = $num1 + $num2;
 
-	echo "O resultado da soma é: ". $resp; 
+	echo "O resultado da soma é: ". $resp;
+	 
  ?>
---
+++ exemplo02.php
@@ -0,0 +1,9 @@
+<?php
+
+	$valor1 = 50;
+	$valor2 = 30;
+	$resposta = ($valor1*$valor2);
+
+	echo("A multiplicação é ". $resposta);
+
+  ?>
